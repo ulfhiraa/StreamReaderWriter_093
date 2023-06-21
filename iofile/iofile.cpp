1c0eca02cd589ac1fe19777ee1d8986e16b634d0
ulfhiraa <studyiraa@gmail.com>
2023-06-21 10:31:44 +0700

    outfile untuk menulis dan << memasukkan nilai 'baris' ke dalam file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -19,5 +19,7 @@ int main() {
 		getline(cin, baris);
 		// loop akan berhenti jika anda memasukkan karakter q
 		if (baris == "q") break;
+		// menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 	}
 }
