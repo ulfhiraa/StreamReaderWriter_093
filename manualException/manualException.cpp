4b55485e6720a6cd72fc8f517bce732cd2d4e0fe
ulfhiraa <studyiraa@gmail.com>
2023-06-21 11:25:30 +0700

    catch untuk menangkap, menampilkan/menunjukkan pesan kesalahan

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -7,4 +7,8 @@ int main() {
 		throw 0.5; // Melemparkan sebuah integer maka
 		cout << "Pertanyaan tidak akan di eksekusi" << endl;
 	}
+	catch (int a) {
+		// blok ini akan di eksekusi
+		cout << "Pengecualian akan di eksekusi" << endl;
+	}
 }
