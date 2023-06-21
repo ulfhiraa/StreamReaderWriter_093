346e2513abff245fd122244d058d24ba4a57ba19
ulfhiraa <studyiraa@gmail.com>
2023-06-21 10:57:13 +0700

    variabel string baris untuk baris dan nama file untuk membuat file baru

--- iofileWithPath/iofileWithPath.cpp
+++ iofileWithPath/iofileWithPath.cpp
@@ -1,2 +1,10 @@
 #include <fstream>
 //untuk membuat, menulis dan membaca input output
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string namaFile;
+}
