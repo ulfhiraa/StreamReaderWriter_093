193399d7d664b56eabc02d29a7ad6d6e109398a1
ulfhiraa <studyiraa@gmail.com>
2023-06-21 11:14:07 +0700

    jika file ada maka if file dibuka, maka melakukan perulangan setiap baris, dan menampilkan baris, dan tutup file tsb apabila sudah selesai

--- iofileWithPath/iofileWithPath.cpp
+++ iofileWithPath/iofileWithPath.cpp
@@ -36,4 +36,19 @@ int main() {
 
 	// menunjuk ke sebuah file
 	infile.open(namaFile, ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		// melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// dan tampilkan disini
+			cout << baris << '\n';
+		}
+		// tutup file tersebut setelah selesai
+		infile.close();
+	}
 }
